597437bfc938f17fd6e8454cfe3a3fa3aa820406
KaneWatson <webitgenius@gmail.com>
2024-03-14 01:23:41 +0100

    added netlify.toml file to avoid site not found error upon refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import Home from "./ui/Home"
-import Error from "./ui/Error"
-import Menu, { loader as menuLoader } from "./features/menu/Menu"
-import Cart from "./features/cart/Cart"
-import Order, { loader as orderLoader } from "./features/order/Order"
-import CreateOrder, { action as createOrderAction } from "./features/order/CreateOrder"
-import AppLayout from "./ui/AppLayout"
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
@@ -14,35 +17,36 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />
+        element: <Home />,
       },
       {
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
-        errorElement: <Error />
+        errorElement: <Error />,
       },
       {
         path: "/cart",
-        element: <Cart />
+        element: <Cart />,
       },
       {
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
-        errorElement: <Error />
+        action: updateOrderAction,
+        errorElement: <Error />,
       },
       {
         path: "/order/new",
         element: <CreateOrder />,
-        action: createOrderAction
-      }
-    ]
-  }
-])
+        action: createOrderAction,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,12 +1,13 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, selectTotalPrice } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
 import { formatCurrency } from "../../utils/helpers";
 import { useState } from "react";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -40,8 +41,16 @@ const fakeCart = [
 
 function CreateOrder() {
   const [priority, setPriority] = useState(false);
-  const username = useSelector((state) => state.user.username);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const isLoadingAddress = addressStatus === "loading";
   const isSubmitting = navigation.state === "submitting";
 
   const formErrors = useActionData();
@@ -84,16 +93,38 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               type="text"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
               className="input w-full"
             />
+            {addressStatus === "error" && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && (
+            <span className="absolute right-[3px] top-[-3px] z-50 sm:top-[2.5px] md:right-[5px] md:top-[5px]">
+              {" "}
+              <Button
+                disabled={isLoadingAddress}
+                type={"small"}
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get Position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -112,7 +143,16 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button type={"primary"} disabled={isSubmitting}>
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.longitude && position.latitude
+                ? `${position.latitude},${position.longitude}`
+                : ""
+            }
+          />
+          <Button type={"primary"} disabled={isSubmitting || isLoadingAddress}>
             {isSubmitting
               ? "Placing order..."
               : `Order now (${formatCurrency(finalPrice)})`}
